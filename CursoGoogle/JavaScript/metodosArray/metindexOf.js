7c73c1675008afbfb705bf35503da5142747ed56
Olderico Cedric Genaro <101076293+cedricgenaro@users.noreply.github.com>
2022-03-20 14:52:21 -0300

    método indexOf

---
+++ CursoGoogle/JavaScript/metodosArray/metindexOf.js
@@ -0,0 +1,3 @@
+let listaLoja = ['Kellogs', 'froot loops', 'nescau', 'snow flakes', 'crunch'];
+let favorito = 'nescau';
+console.log(listaLoja.includes(favorito) ? listaLoja.indexOf(favorito) : 'Seu cereal não foi encontrado!');
