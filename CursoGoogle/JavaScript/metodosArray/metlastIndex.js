9956582004b83ed8080e10a9e8d67dea858163bf
Olderico Cedric Genaro <101076293+cedricgenaro@users.noreply.github.com>
2022-03-20 15:08:05 -0300

    metodoLastIndex

---
+++ CursoGoogle/JavaScript/metodosArray/metlastIndex.js
@@ -0,0 +1,4 @@
+let compras = ['leite', 'farinha', 'oleo', 'açucar', 'leite'];
+console.log(compras);
+let index = compras.lastIndexOf('leite');
+console.log(`O item extra registrado está no indice ${index}`);
