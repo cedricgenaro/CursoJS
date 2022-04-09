80d595f156adacd8893543cc6aed31508a702e97
Olderico Cedric Genaro <101076293+cedricgenaro@users.noreply.github.com>
2022-04-09 17:30:00 -0300

    funções como argumentos

--- CursoGoogle/JavaScriptD3/manipEv/js/javascript.js
+++ CursoGoogle/JavaScriptD3/manipEv/js/javascript.js
@@ -1,14 +1,30 @@
 
 var svg = d3.select('body').append('svg').attr('width', 600).attr('height', 500);
-var balloon = svg.append('circle').attr('fill', '#ff0000').attr('r', 50).attr('cx', 350).attr('cy', 250);
 
-function pickRandom(max){
-  return  Math.random() * (max - 50) + 50;
+var int1 =  svg.append('rect').attr('width', 40).attr('height', 70).attr('fill', 'white').attr('x', 50).attr('y', 30).attr('stroke', 'grey');
+
+var int2 = svg.append('rect').attr('width', 30).attr('height', 60).attr('fill', 'lightgrey').attr('x', 55).attr('y', 35);
+
+var bt1 = svg.append('rect').attr('width', 30).attr('height', 30).attr('fill', 'grey').attr('x', 55).attr('y', 65);
+
+var fio = svg.append('rect').attr('width', 2).attr('height', 100).attr('fill', 'grey').attr('x', 180);
+
+var bocal1 = svg.append('rect').attr('width', 30).attr('height',5).attr('fill', 'white').attr('stroke', 'grey').attr('x', 50).attr('y', 100).attr('x', 165);
+
+var bocal2 = svg.append('rect').attr('width', 30).attr('height',5).attr('fill', 'white').attr('stroke', 'grey').attr('x', 50).attr('y', 107).attr('x', 165);
+
+var bocal3 = svg.append('rect').attr('width', 30).attr('height',5).attr('fill', 'white').attr('stroke', 'grey').attr('x', 50).attr('y', 114).attr('x', 165);
+
+var lamp = svg.append('circle').attr('r',30).attr('cx', 180).attr('cy', 152);
+function acender() {
+  bt1.attr('y', 35);
+  lamp.attr('fill', 'yellow');
 }
 
-function randomSize() {
-    balloon.attr('r', pickRandom(200));
+function apagar() {
+  bt1.attr('y', 65);
+  lamp.attr('fill', 'black');
 }
 
-balloon.on('click', randomSize);
-
+bt1.on('click', acender);
+int2.on('click', apagar);
