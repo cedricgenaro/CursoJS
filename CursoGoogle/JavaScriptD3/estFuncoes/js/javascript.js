5cb9e17c034b665691799cbfc000a3aad45e25dd
Olderico Cedric Genaro <101076293+cedricgenaro@users.noreply.github.com>
2022-04-17 19:20:12 -0300

    Estudo de transição

--- CursoGoogle/JavaScriptD3/estFuncoes/js/javascript.js
+++ CursoGoogle/JavaScriptD3/estFuncoes/js/javascript.js
@@ -3,11 +3,12 @@ var svg = d3.select('body').append('svg').attr('width', 600).attr('height', 500)
 var balloon = svg.append('circle').attr('fill', '#ff0000').attr('r', 50).attr('cx', 350).attr('cy', 250);
 
 function pickRandom(max){
-  return  Math.random() * (max - 50) + 50;
+  var ar = balloon.attr('r')
+  return   Math.random() * (max - 100) + 100 ;
 }
 
 function randomSize() {
-    balloon.attr('r', pickRandom(200));
+    balloon.transition().duration(9000).attr('r', pickRandom(200));
 }
 
 balloon.on('click', randomSize);
--- CursoGoogle/JavaScriptD3/path/js/javascript.js
+++ CursoGoogle/JavaScriptD3/path/js/javascript.js
@@ -1,7 +1,27 @@
 
 var svg = d3.select('body').append('svg').attr('width', 600).attr('height', 500);
 
+function aleatorio(){
+   var pos = Math.random(1,600)* 500
+   return pos;
+} 
+function sumir() {
+    contafinal.remove();
+}
 
+function cura() {
+    bacterias.remove();
+}
+
+
+function mult() {
+    for (var i = 0; i <= 50; i++){
+        svg.append('circle').attr('cx', aleatorio).attr('cy', aleatorio).attr('r', 1).attr('fill', 'yellowgreen');
+    }
+}
+mult();
+aleatorio()
+svg.selectAll('circle').append('animate').attr('attributeName', 'r').attr('values', '0;100;100').attr('dur', '60s');
 var contagotas = svg.append('g')
 
 
@@ -10,10 +30,16 @@ var liq = svg.select('g').append('path').attr('d','M 12,48 C12,50 10,52 10,60 L8
 var frasco = svg.select('g').append('path').attr('d','M6,12 C6,0 12,0 12,12 C14,12 14,18 12,18 L12,48 C12,50 10,52 10,60 L8,60 C8,52 6,50 6,48 L6,18 C4,18 4,12 6,12 Z').attr('fill', 'silver').attr('stroke', 'GREY').attr('stroke-width', 0,2).attr('opacity', 0.5);
 
 var cabeca = svg.select('g').append('path').attr('d','M6,12 C6,0 12,0 12,12 C14,12 14,18 12,18 L6,18 C4,18 4,12 6,12 Z').attr('stroke', 'GREY').attr('stroke-width', 0,2);
+contagotas.attr('transform', 'scale(4) translate(30 25) rotate(-45 0 0)');
 
-var contafinal = contagotas.attr('transform', 'scale(3) translate(30 15) rotate(-45 0 0)');
+var bacterias = svg.selectAll('circle');
+var contafinal = svg.select('g');
+
+contafinal.on('click', sumir);
+bacterias.on('click', cura);
+
 /* contagotas.append('animateTransform').attr('attributeName', 'transform').attr('type', 'scale').attr('from', '0').attr('to', 50).attr('dur', '60s'); */
 //contafinal.append('animateTransform').attr('attributeName', 'transform').attr('type', 'rotate').attr('from', '0 0 0').attr('to', '-45 0 0').attr('dur', '10s');
-svg.select('g').append('animateTransform').attr('attributeName', 'transform').attr('type', 'scale').attr('from', '3').attr('to', 50).attr('dur', '6s');
-
-/* <path d="M 12 48 C 12 50 10 52 10 60 L 8 60 C8 52 6 50 6 48 Z" fill="yellowgreen" opacity="1"></path> */
+/* svg.select('g').append('animateTransform').attr('attributeName', 'transform').attr('type', 'scale').attr('from', '3').attr('to', 50).attr('dur', '6s');
+ */
+console.log(bacterias.attr('fill'));
