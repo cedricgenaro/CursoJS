7d6375b7c468cc02292624dd294bcb5c745b8168
Olderico Cedric Genaro <101076293+cedricgenaro@users.noreply.github.com>
2022-06-04 16:42:34 -0300

    Criei o aula 10 de JavaScript Vanilla

---
+++ Curso Vanilla JS/aula10 Arrays/javascript/javascript.js
@@ -0,0 +1,15 @@
+//Formal 
+let myArr = new Array("Thiago", "webdesign", 10);
+
+
+//Literal
+let myArr2 = ["Thais", "teacher", {school: "center"}];
+
+//Associativo
+let myArrAss = {
+    name: 'thiago',
+    age: 18
+}
+myArr2[3] = 139339;
+console.log(myArr.prototype);
+
