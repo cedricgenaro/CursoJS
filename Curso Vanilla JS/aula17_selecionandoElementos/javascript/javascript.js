fb0c8533353f685afce313bfa134d0fd1c68f482
Olderico Cedric Genaro <101076293+cedricgenaro@users.noreply.github.com>
2022-06-25 18:15:52 -0300

    criei aula 17 de JavaScript Vanilla

--- Curso Vanilla JS/aula17_selecionandoElementos/javascript/javascript.js
+++ Curso Vanilla JS/aula17_selecionandoElementos/javascript/javascript.js
@@ -1,12 +1,26 @@
-//Caixa de alert
-/*
- alert('Esse é um alerta!');
- */
+(function readyJS(win, doc) {
+    'use strict';
 
-//Caixa de dialogo
-/* 
-confirm('Deseja mesmo apagar esse dado? \n Esse dado será excluido!');
- */
+   /*  let teste = doc.getElementById('teste');
+    teste.className = 'New Class';
+    teste.style.color = 'pink';
+    console.log(teste); */
 
-//Caixa de prompt
-prompt('Qual sua data de nascimento?','dd/mm/yyyy' );
+   /*  let myClass = doc.getElementsByClassName('btn');
+    console.log(myClass[0].innerHTML); */
+    /* let byName = doc.getElementsByName('img');
+    console.log(byName); */
+   /*  let byTag= doc.getElementsByTagName('h1');
+    console.log(byTag); */
+   let byQuery = doc.querySelector('.btn');
+  /*   byQuery.addEventListener('click', function(){
+        alert('oi');
+    });   */
+
+    let byQueryAll = doc.querySelectorAll('.btn');
+    for(let i = 0; i < byQueryAll.length; i++) {
+        byQueryAll[i].addEventListener('click', function(){
+            doc.querySelector('body').style.backgroundColor = this.getAttribute('data-color');
+        });
+    }
+})(window, document);
