a9d5f9a185f04297f17d74a7d5534b2339c419fd
Olderico Cedric Genaro <101076293+cedricgenaro@users.noreply.github.com>
2022-07-03 16:12:37 -0300

    minijogo d3 Js

---
+++ CursoGoogle/JavaScriptD3/comecarTopo/js/javascript.js
@@ -0,0 +1,25 @@
+var svg = d3.select('body').append('svg').attr('width', 400).attr('height', 500);
+
+var position = [];
+
+
+if(position.length == 0) {
+
+    for(let i = 35; i <= 400 - 35; i+=5) {
+        
+        position.push(i);
+    }
+}
+
+function pickPosition(arr){
+    let posicao = arr[Math.floor(Math.random() * arr.length)];
+    return posicao;
+}
+
+
+
+var chao = svg.append('rect').attr('width', window.innerWidth).attr('height', window.innerHeight - 200).attr('fill', 'silver').attr('y', 190 + 35);
+
+let bol = svg.append('circle').attr('r', 35).attr('cx', pickPosition(position) ).attr('cy', 35).attr('fill', 'red');
+
+ 
