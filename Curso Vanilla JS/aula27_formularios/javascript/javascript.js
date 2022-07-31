a4292f0870e247eeb6b40c3803dd2957a68288f3
Olderico Cedric Genaro <101076293+cedricgenaro@users.noreply.github.com>
2022-07-31 19:48:23 -0300

    Criei a aula 27 JS Vanilla

---
+++ Curso Vanilla JS/aula27_formularios/javascript/javascript.js
@@ -0,0 +1,32 @@
+(function readyJs(win, doc){
+    'use strict';
+
+   let gallery = doc.querySelector('.gallery');
+   let files = doc.querySelector('#img');
+
+   function imgPreview(event){
+      gallery.innerHTML = '';  
+      let files = event.target.files;
+      for(let i = 0; i < files.length; i++) {
+        if(files[i].size >  300000 ){
+            alert('Imagem muito grande!');
+        }else{
+            let reader = new FileReader();
+            reader.onload = function(event){
+                let urlImg = event.target.result;
+                let newImg = doc.createElement('img');
+                newImg.setAttribute('src', urlImg);
+                newImg.style = 'width: 100px; margin: 5px;' 
+                gallery.appendChild(newImg);
+            }
+         reader.readAsDataURL(files[i]);
+        }
+         
+      }
+        
+   }
+   
+   files.addEventListener('change', imgPreview, false);
+
+
+})(window, document);
